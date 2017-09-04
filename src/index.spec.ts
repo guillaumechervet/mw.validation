7c18179e5f23e54bf6eda7b96922afd0a5373e5f
guillaume chervet <guillaume.chervet@gmail.com>
2017-09-04 13:46:28 +0200

    fix(deleted code): to much code deleted

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,11 +1,41 @@
 ﻿import { expect } from 'chai';
 import 'mocha';
-// if you used the '@types/mocha' method to install mocha type definitions, uncomment the following line
-// import 'mocha';
+import { validation } from './index';
 
 describe('index', () => {
-  /* it('should return hello world', () => {
-    const result = hello();
-    expect(result).to.equal('Hello World!');
-  });*/
+  it('color', () => {
+    var rules = {
+      input: ['color']
+    };
+
+    //#abc and #abcdef   but not #abcd
+
+    var result = validation.validateView("#abc", rules.input);
+    console.log(result);
+    //expect(result[0].success).to.equal(true);
+    result = validation.validateView("#abcdef", rules.input);
+    console.log(result);
+    expect(result[0].success).to.equal(true);
+    result = validation.validateView("#abcd", rules.input);
+    expect(result[0].success).to.equal(true);
+    result = validation.validateView("abcdef", rules.input);
+    expect(result[0].success).to.equal(false);
+
+  });
+  it('require', () => {
+
+    (function () {
+      var rules = {
+        input: [{
+          'required': {
+            message: 'New error message'
+          }
+        }]
+      };
+
+      var result = validation.validateView("toto", rules.input);
+      console.log("Assert : ", result[0].success);
+    })();
+
+  });
 });
--- src/validation/object/validateObject.ts
+++ src/validation/object/validateObject.ts
@@ -2,61 +2,110 @@
 import * as validation from "../validateRules";
 
 function validateModelInternal(model, rules, result, key, isStrict) {
-      if (!rules) {
-        return result;
-      }
+  if (!rules) {
+    return result;
+  }
 
-      for (let name in model) {
-        let value = model[name];
-        if (rules[name]) {
-          let valResults = validation.validateModel(value, rules[name]);
-          for (var i = 0; i < valResults.length; i++) {
-            var valResult = valResults[i];
-            if (!valResult.success) {
-              result.success = false;
-              var info = {};
-              result.detail[key + '.' + name + '.' + valResult.name] = valResult.message;
-            }
-          }
-        } else if (isStrict) {
-          var subRules = rules['@' + name];
-          if (!subRules || typeof subRules !== 'object') {
-            result.detail[key + '.' + name + '.illegal'] = 'La proprieté n\'est pas authorisée.';
-          }
+  for (let name in model) {
+    let value = model[name];
+    if (rules[name]) {
+      let valResults = validation.validateModel(value, rules[name]);
+      for (var i = 0; i < valResults.length; i++) {
+        const valResult = valResults[i];
+        if (!valResult.success) {
+          result.success = false;
+          var info = {};
+          result.detail[key + '.' + name + '.' + valResult.name] = valResult.message;
         }
       }
-
-      for (let name in model) {
-        let value = model[name];
-        if (typeof value === 'object') {
-          validateModelInternal(value, rules['@' + name], result, key + '.' + name, false);
-        } else if (Object.prototype.toString.call(value) === '[object Array]') {
-          for (var i = 0; i < value.length; i++) {
-            validateModelInternal(value, rules['[]' + name], result, key + '.' + name + '[' + i + ']', false);
-          }
-        }
+    } else if (isStrict) {
+      const subRules = rules['@' + name];
+      if (!subRules || typeof subRules !== 'object') {
+        result.detail[key + '.' + name + '.illegal'] = 'La proprieté n\'est pas authorisée.';
       }
-
-      // Detect non present property
-       for (let name in rules) {
-          name = name.replace('@', '');
-          var propertyValue = model[name];
-          if(propertyValue === undefined){
-             result.detail[key + '.' + name + '.notfound'] = 'La proprieté n\'est pas présente.';
-          }
-       }
-
-      return result;
     }
+  }
 
-    function validateModel(model, rules, isStrict=false) {
-
-      var result = { success: true, detail: {} };
-      var key = 'model';
-      validateModelInternal(model, rules, result, key, isStrict);
-      return result;
+  for (let name in model) {
+    let value = model[name];
+    if (typeof value === 'object') {
+      validateModelInternal(value, rules['@' + name], result, key + '.' + name, false);
+    } else if (Object.prototype.toString.call(value) === '[object Array]') {
+      for (var i = 0; i < value.length; i++) {
+        validateModelInternal(value, rules['[]' + name], result, key + '.' + name + '[' + i + ']', false);
+      }
     }
+  }
 
-    export {
-        validateModel
-    };
+  // Detect non present property
+  for (let name in rules) {
+    name = name.replace('@', '');
+    var propertyValue = model[name];
+    if (propertyValue === undefined) {
+      result.detail[key + '.' + name + '.notfound'] = 'La proprieté n\'est pas présente.';
+    }
+  }
+
+  return result;
+}
+
+function validateModel(model, rules, isStrict = false) {
+  const result = { success: true, detail: {} };
+  const key = 'model';
+  validateModelInternal(model, rules, result, key, isStrict);
+  return result;
+}
+
+// Liste toutes les fonctions d'un objet (parcour tout l'objet en recurssif)
+function getFunctions(inputObject, functions=null) {
+  if (functions === void 0) { functions = undefined; }
+  if (!functions) {
+    functions = [];
+  }
+  if (inputObject instanceof Array) {
+    // On recherche s'il y a un onlyIf générale sur toute les règles associées
+    for (var i = 0; i < inputObject.length; i++) {
+      var newInputObject = inputObject[i];
+      getFunctions(newInputObject, functions);
+    }
+  }
+  else if (typeof inputObject === 'string') {
+    return functions;
+  }
+  else if (typeof inputObject === 'object') {
+    for (const name in inputObject) {
+      // Cas particulié de la règle customs ejecté
+      if (name === 'validateView') {
+        functions.push({ name: name, func: inputObject.validateView });
+        continue;
+      }
+      if (name === 'validateModel') {
+        functions.push({ name: name, func: inputObject.validateModel });
+        continue;
+      }
+      getFunctions(inputObject[name], functions);
+    }
+  }
+  else if (typeof inputObject === 'function') {
+    functions.push({ name: "function", func: inputObject });
+  }
+  return functions;
+}
+
+function getFunctionsResult(inputObject, results) {
+  const functions = getFunctions(inputObject);
+  if (!results) {
+    results = {};
+  }
+  const l = functions.length;
+  for (var i = 0; i < l; i++) {
+    results[i.toString()] = functions.func[i]();
+  }
+  return results;
+}
+
+export {
+  validateModel,
+  getFunctions,
+  getFunctionsResult
+};
