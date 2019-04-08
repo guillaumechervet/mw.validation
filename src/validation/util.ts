664d5a6112b72911e7e773f443b7825cd0956d13
Olivier YOUF <olivier.youf@axa.fr>
2019-04-08 10:47:41 +0200

    fix : ssn validation key < 10

--- src/validation/util.ts
+++ src/validation/util.ts
@@ -100,6 +100,13 @@
     }
     return newHashTable;
   }
+  public formatNumberLength(num:number, length:number):string {
+    var r = "" + num;
+    while (r.length < length) {
+        r = "0" + r;
+    }
+    return r;
+  }
 }
 
 var util = new Util();
