c9f10cc94e23431af30279e24d957f80556aa717
Guillaume Chervet <guillaume.chervet@axa.fr>
2019-05-03 16:19:33 +0000

    maj

--- src/validation/i18n/textFormatter.ts
+++ src/validation/i18n/textFormatter.ts
@@ -1,16 +1,19 @@
-﻿
-    var format = function (format: string, ...args) {
-        //var args = Array.prototype.slice.call(arguments, 1);
-        return format.replace(/{(\d+)}/g, function (match, number) {
-            return typeof args[number] !== 'undefined'
-        ? args[number]
-        : match
-      ;
-        });
-    };
+﻿const format = function(format: string, ...args) {
+  return format.replace(/{(\d+)}/g, function(match, number) {
+    return typeof args[number] !== "undefined" ? args[number] : match;
+  });
+};
 
-    function endWith(str, suffix) {
-        return str.indexOf(suffix, str.length - suffix.length) !== -1;
-    }
+const template = function(format: string, data) {
+  if (!data) return format;
+  Object.keys(data).forEach(key => {
+    format = format.replace("{" + key + "}", data[key]);
+  });
+  return format;
+};
 
-    export {format, endWith};
+function endWith(str, suffix) {
+  return str.indexOf(suffix, str.length - suffix.length) !== -1;
+}
+
+export { format, endWith, template };
